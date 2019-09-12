1b29eb6e4c123f863d8994188245f22ad7f6251b
AndrewLam97 <alam1997@gmail.com>
2019-09-12 16:41:48 -0700

    Initial Files

---
+++ Classwork/src/Classwork.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Classwork.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age = 0;
+	cout << "What's your age?" << endl;
+	cin >> age;
+	cout << "You are " << age << " years old. \n";
+	return 0;
+}
