ca3e9c7e028bc67fb826a5d4bf656f39d6ca4070
AndrewLam97 <alam1997@gmail.com>
2019-09-26 17:59:57 -0700

    Update GuessingGame.cpp

--- GuessingGame/GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame/GuessingGame.cpp
@@ -7,13 +7,13 @@
 #include <ctime>
 using namespace std;
 
+//takes lower and upper bounds and returns a random number inclusive
 int guesser(int low, int high)
 {
 	mt19937 mt_rand(time(0));
 	uniform_int_distribution<int> gen(low, high);
 	int randomNum = gen(mt_rand);
-	cout << "guesser guessed " << randomNum << endl;
-
+	//cout << "guesser guessed " << randomNum << endl;
 	return randomNum;
 }
 
@@ -23,7 +23,7 @@ int main()
 	int low = 1, high = 100, guess = 1;
 	cout << "Think of a number between 1 and 100 and press enter to proceed" << endl;
 	cin.ignore();
-
+	//Takes user input until program receives a 'y' by guessing the right number
 	do
 	{
 		char entry;
@@ -35,16 +35,16 @@ int main()
 		cout << "Did I get it? Y/N" << endl;
 		cin >> entry;
 
-		if (entry == 'y' || entry == 'Y')
+		if (entry == 'y' || entry == 'Y') //guessed correctly
 		{
 			cout << "Yay I got it!";
 			break;
 		}
-		else
+		else //guessed incorrectly
 		{
-			cout << "Is it lower than " << guess << "? Y/N" << endl;
+			cout << "Is it lower than " << guess << "? Y/N" << endl; //evaluates higher or lower than guess
 			cin >> lowOrHigh;
-			if (lowOrHigh == 'y' || lowOrHigh == 'Y')
+			if (lowOrHigh == 'y' || lowOrHigh == 'Y')//updates boundaries depending if response was higher/lower
 			{
 				high = guess - 1;
 			}
@@ -57,4 +57,3 @@ int main()
 	} while (true);
 
 }
-
