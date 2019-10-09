d3cbe9da743e0ccb0f7db9c8f124f430bad71d8e
AndrewLam97 <alam1997@gmail.com>
2019-10-08 17:38:37 -0700

    update CS002_RomanConverter and add CS002_Struct

--- CS002_RomanConverter/CS002_RomanConverter/CS002_RomanConverter.cpp
+++ CS002_RomanConverter/CS002_RomanConverter/CS002_RomanConverter.cpp
@@ -35,48 +35,116 @@ string romanConvert(int x)
 		return "M";
 }
 
+string modToString(string place, int value)
+{
+	return 0;
+}
+
 int main()
 {
-	
-	string entry;
-	/*
-	struct arabicNum
+	while (true)
 	{
-		int thousands;
-		int hundreds;
-		int tens;
-		int ones;
-	};
+		string entry;
+		string result = "";
 
-	while(true)
-	{
-		string result = ""; */
 		cout << "Enter Arabic numeral or done: ";
-		cin >> entry; 
+		cin >> entry;
 		if (entry == "done")
 		{
-			exit;
+			break;
 		}
 
 		int entryNum = stoi(entry);
-		/*
-		arabicNum num;
 
-		for (int i = 0; i < entry.length(); i++)
-		{
-			result += romanConvert(entry[i]);
-		}
-
-		cout << "Your Roman Numeral is: " << result << endl;
-
-	}*/
-
-
-	for (int i = 12; i > 0; i--)
-	{
 		while (entryNum > 0)
 		{
+			for (int thousands = entryNum / 1000; thousands > 0; thousands--)
+			{
+				cout << romanConvert(1000);
+				entryNum -= 1000;
+			}
 
+			int hundreds = entryNum / 100;
+			entryNum -= hundreds * 100;
+			do
+			{
+				if (hundreds == 9)
+				{
+					cout << romanConvert(900);
+					hundreds -= 9;
+				}
+				else if (hundreds >= 5)
+				{
+					cout << romanConvert(500);
+					hundreds -= 5;
+				}
+				else if (hundreds == 4)
+				{
+					cout << romanConvert(400);
+					hundreds -= 4;
+				}
+				else if (hundreds < 4 && hundreds > 0)
+				{
+					cout << romanConvert(100);
+					hundreds -= 1;
+				}
+				else break;
+			}while(hundreds > 0);
+
+			int tens = entryNum / 10;
+			entryNum -= tens * 10;
+			do
+			{
+				if (tens == 9)
+				{
+					cout << romanConvert(90);
+					tens -= 9;
+				}
+				else if (tens >= 5)
+				{
+					cout << romanConvert(50);
+					tens -= 5;
+				}
+				else if (tens == 4)
+				{
+					cout << romanConvert(40);
+					tens -= 4;
+				}
+				else if (tens < 4 && tens > 0)
+				{
+					cout << romanConvert(10);
+					tens -= 1;
+				}
+				else break;
+			} while (tens > 0);
+
+			int ones = entryNum / 1;
+			entryNum -= ones;
+			do
+			{
+				if (ones == 9)
+				{
+					cout << romanConvert(9);
+					ones -= 9;
+				}
+				else if (ones >= 5)
+				{
+					cout << romanConvert(5);
+					ones -= 5;
+				}
+				else if (ones == 4)
+				{
+					cout << romanConvert(4);
+					ones -= 4;
+				}
+				else if (ones < 4 && ones > 0)
+				{
+					cout << romanConvert(1);
+					ones -= 1;
+				}
+				else break;
+			} while (ones > 0);
 		}
+		cout << endl;
 	}
 }
