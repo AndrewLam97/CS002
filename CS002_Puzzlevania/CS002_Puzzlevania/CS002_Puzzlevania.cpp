6820a883127a01eff1e4f9e58a36e8b8f9dd034b
AndrewLam97 <alam1997@gmail.com>
2019-10-14 15:29:38 -0700

    Add CS002_Puzzlevania

---
+++ CS002_Puzzlevania/CS002_Puzzlevania/CS002_Puzzlevania.cpp
@@ -0,0 +1,20 @@
+/*
+CS002_Puzzlevania.cpp
+
+*/
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+void shoot(bool& targetAlive, double accuracy);
+
+int main()
+{
+
+}
+
+void shoot(bool& targetAlive, double accuracy)
+{
+
+}
