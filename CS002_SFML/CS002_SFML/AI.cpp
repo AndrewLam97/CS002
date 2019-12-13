c0ad0d1f390cb7d6b1271e8fd37f6c6377f1a984
AndrewLam97 <alam1997@gmail.com>
2019-12-12 18:09:49 -0800

    Update AI.cpp

--- CS002_SFML/CS002_SFML/AI.cpp
+++ CS002_SFML/CS002_SFML/AI.cpp
@@ -2,17 +2,6 @@
 
 Move AI::AIturn(Board board)
 {
-	/*for (int i = 0; i < board.getROWS(); i++)
-	{
-		for (int j = 0; j < board.getCOLS(); j++)
-		{
-			Move move = { i,j,'o' };
-			if (board.isValidMove(move))
-			{
-				return move;
-			}
-		}
-	}*/
 	return miniMax(board);
 }
 
